c43425ae2bfd1f3483f5c1719f3b7eacfbdb4c51
Shishir707 <mirshr8@gmail.com>
2025-11-04 01:40:19 +0600

    added assets path

---
+++ lib/UI/utils/assets_path.dart
@@ -0,0 +1,6 @@
+class AssetsPaths {
+  static const String _imageURl = 'assets/images';
+
+  static const String backgroundSvg = '$_imageURl/background.svg';
+  static const String logoSvg = '$_imageURl/logo.svg';
+}
