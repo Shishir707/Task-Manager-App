3456175ee1f68f4430eb3ee69cafc64d9a1089ab
Shishir707 <mirshr8@gmail.com>
2025-11-14 01:13:43 +0600

    make card to widget

--- lib/UI/screen/new_task_screen_list.dart
+++ lib/UI/screen/new_task_screen_list.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:task_manager/UI/widgets/task_card.dart';
 
 class NewTaskScreenList extends StatefulWidget {
   const NewTaskScreenList({super.key});
@@ -16,68 +17,14 @@ class _NewTaskScreenListState extends State<NewTaskScreenList> {
           _NewSummaryList(),
           SizedBox(height: 8,),
           Expanded(
-            child: ListView.separated(
-              itemCount: 15,
-              itemBuilder: (context, index) {
-                return ListTile(
-                  tileColor: Colors.white,
-                  title: Text('New Task'),
-                  subtitle: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text('Description of the task'),
-                      Text(
-                        'Date: 29 Jun 2022',
-                        style: TextStyle(color: Colors.grey),
-                      ),
-                      SizedBox(height: 10),
-                      Row(
-                        children: [
-                          Container(
-                            padding: EdgeInsets.symmetric(
-                              horizontal: 16,
-                              vertical: 4,
-                            ),
-                            decoration: BoxDecoration(
-                              color: Colors.green,
-                              borderRadius: BorderRadius.circular(16),
-                            ),
-                            child: Text(
-                              'New',
-                              style: TextStyle(
-                                color: Colors.white,
-                                fontWeight: FontWeight.w600,
-                              ),
-                            ),
-                          ),
-                          SizedBox(width: 210),
-                          IconButton(
-                            onPressed: () {},
-                            icon: Icon(
-                              Icons.delete,
-                              color: Colors.red.shade300,
-                            ),
-                          ),
-                          IconButton(
-                            onPressed: () {},
-                            icon: Icon(Icons.edit, color: Colors.blue.shade300),
-                          ),
-                        ],
-                      ),
-                    ],
-                  ),
-                );
-              },
-              separatorBuilder: (BuildContext context, int index) {
-                return SizedBox(height: 8);
-              },
-            ),
+            child: TaskCard(),
           ),
         ],
       ),
     );
   }
 }
+
 
 class _NewSummaryList extends StatelessWidget {
   const _NewSummaryList();
