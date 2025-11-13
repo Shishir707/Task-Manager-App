7a34a6da828e8205548266024bfa923aacf59d13
Shishir707 <mirshr8@gmail.com>
2025-11-14 00:53:33 +0600

    show task listTile

--- lib/UI/screen/new_task_screen_list.dart
+++ lib/UI/screen/new_task_screen_list.dart
@@ -13,27 +13,98 @@ class _NewTaskScreenListState extends State<NewTaskScreenList> {
     return Scaffold(
       body: Column(
         children: [
-          SizedBox(
-            height: 68,
-            child: ListView.builder(
-              itemCount: 10,
-              scrollDirection: Axis.horizontal,
+          _NewSummaryList(),
+          SizedBox(height: 8,),
+          Expanded(
+            child: ListView.separated(
+              itemCount: 15,
               itemBuilder: (context, index) {
-                return Card(
-                  color: Colors.white,
-                  elevation: 0,
-                  margin: EdgeInsets.only(left: 5,top: 5),
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-                    child: Column(children: [Text('12',style: TextTheme.of(context).titleLarge,),
-                      Text('New',style: TextTheme.of(context).labelSmall,)
-                    ]),
+                return ListTile(
+                  tileColor: Colors.white,
+                  title: Text('New Task'),
+                  subtitle: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text('Description of the task'),
+                      Text(
+                        'Date: 29 Jun 2022',
+                        style: TextStyle(color: Colors.grey),
+                      ),
+                      SizedBox(height: 10),
+                      Row(
+                        children: [
+                          Container(
+                            padding: EdgeInsets.symmetric(
+                              horizontal: 16,
+                              vertical: 4,
+                            ),
+                            decoration: BoxDecoration(
+                              color: Colors.green,
+                              borderRadius: BorderRadius.circular(16),
+                            ),
+                            child: Text(
+                              'New',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.w600,
+                              ),
+                            ),
+                          ),
+                          SizedBox(width: 210),
+                          IconButton(
+                            onPressed: () {},
+                            icon: Icon(
+                              Icons.delete,
+                              color: Colors.red.shade300,
+                            ),
+                          ),
+                          IconButton(
+                            onPressed: () {},
+                            icon: Icon(Icons.edit, color: Colors.blue.shade300),
+                          ),
+                        ],
+                      ),
+                    ],
                   ),
                 );
+              },
+              separatorBuilder: (BuildContext context, int index) {
+                return SizedBox(height: 8);
               },
             ),
           ),
         ],
+      ),
+    );
+  }
+}
+
+class _NewSummaryList extends StatelessWidget {
+  const _NewSummaryList({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 68,
+      child: ListView.builder(
+        itemCount: 10,
+        scrollDirection: Axis.horizontal,
+        itemBuilder: (context, index) {
+          return Card(
+            color: Colors.white,
+            elevation: 0,
+            margin: EdgeInsets.only(left: 5, top: 5),
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+              child: Column(
+                children: [
+                  Text('12', style: TextTheme.of(context).titleLarge),
+                  Text('New', style: TextTheme.of(context).labelSmall),
+                ],
+              ),
+            ),
+          );
+        },
       ),
     );
   }
