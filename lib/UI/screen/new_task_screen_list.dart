c289c740645a9b5c82dbbb1ea08663b513146fa6
Shishir707 <mirshr8@gmail.com>
2025-11-17 21:41:41 +0600

    change task_card for make scrollable

--- lib/UI/screen/new_task_screen_list.dart
+++ lib/UI/screen/new_task_screen_list.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:task_manager/UI/widgets/task_card.dart';
+import '../widgets/task_card.dart';
 
 class NewTaskScreenList extends StatefulWidget {
   const NewTaskScreenList({super.key});
@@ -12,12 +12,25 @@ class _NewTaskScreenListState extends State<NewTaskScreenList> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          _NewSummaryList(),
-          SizedBox(height: 8),
-          Expanded(child: TaskCard()),
-        ],
+      body: SingleChildScrollView(
+        child: Column(
+          spacing: 8,
+          children: [
+            const SizedBox(),
+            _buildTaskSummaryListView(),
+            ListView.separated(
+              itemCount: 10,
+              primary: false,
+              shrinkWrap: true,
+              itemBuilder: (context, index) {
+                return TaskCard();
+              },
+              separatorBuilder: (context, index) {
+                return SizedBox(height: 8);
+              },
+            ),
+          ],
+        ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: _onTapAddNewTaskButton,
@@ -27,30 +40,28 @@ class _NewTaskScreenListState extends State<NewTaskScreenList> {
   }
 
   void _onTapAddNewTaskButton() {
-    Navigator.pushNamed(context, "/add-new");
+    Navigator.pushNamed(context, '/add-new');
   }
-}
 
-class _NewSummaryList extends StatelessWidget {
-  const _NewSummaryList();
-
-  @override
-  Widget build(BuildContext context) {
+  Widget _buildTaskSummaryListView() {
     return SizedBox(
-      height: 68,
+      height: 60,
       child: ListView.builder(
         itemCount: 10,
         scrollDirection: Axis.horizontal,
         itemBuilder: (context, index) {
           return Card(
-            color: Colors.white,
             elevation: 0,
-            margin: EdgeInsets.only(left: 5, top: 5),
+            color: Colors.white,
+            margin: EdgeInsets.only(left: 8),
             child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+              padding: const EdgeInsets.symmetric(
+                horizontal: 16.0,
+                vertical: 8,
+              ),
               child: Column(
                 children: [
-                  Text('12', style: TextTheme.of(context).titleLarge),
+                  Text('12', style: TextTheme.of(context).titleMedium),
                   Text('New', style: TextTheme.of(context).labelSmall),
                 ],
               ),
--- lib/UI/widgets/task_card.dart
+++ lib/UI/widgets/task_card.dart
@@ -1,67 +1,53 @@
 import 'package:flutter/material.dart';
 
 class TaskCard extends StatelessWidget {
-  const TaskCard({
-    super.key,
-  });
+  const TaskCard({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return ListView.separated(
-      itemCount: 15,
-      itemBuilder: (context, index) {
-        return ListTile(
-          tileColor: Colors.white,
-          title: Text('Tittle of task'),
-          subtitle: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text('Description of the task'),
-              Text(
-                'Date: 29 Jun 2022',
-                style: TextStyle(color: Colors.grey),
-              ),
-              SizedBox(height: 10),
-              Row(
-                children: [
-                  Container(
-                    padding: EdgeInsets.symmetric(
-                      horizontal: 16,
-                      vertical: 4,
-                    ),
-                    decoration: BoxDecoration(
-                      color: Colors.green,
-                      borderRadius: BorderRadius.circular(16),
-                    ),
-                    child: Text(
-                      'New',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontWeight: FontWeight.w600,
-                      ),
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 16),
+      child: ListTile(
+        tileColor: Colors.white,
+        title: Text('Title of task'),
+        subtitle: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Description of the task',
+              style: TextStyle(color: Colors.grey),
+            ),
+            Text('Date: 12 June 2021'),
+            Row(
+              children: [
+                Container(
+                  padding: EdgeInsets.symmetric(horizontal: 16, vertical: 4),
+                  decoration: BoxDecoration(
+                    color: Colors.green,
+                    borderRadius: BorderRadius.circular(16),
+                  ),
+                  child: Text(
+                    'New',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.w600,
                     ),
                   ),
-                  SizedBox(width: 210),
-                  IconButton(
-                    onPressed: () {},
-                    icon: Icon(
-                      Icons.delete,
-                      color: Colors.red.shade300,
-                    ),
-                  ),
-                  IconButton(
-                    onPressed: () {},
-                    icon: Icon(Icons.edit, color: Colors.blue.shade300),
-                  ),
-                ],
-              ),
-            ],
-          ),
-        );
-      },
-      separatorBuilder: (BuildContext context, int index) {
-        return SizedBox(height: 8);
-      },
+                ),
+                Spacer(),
+                IconButton(
+                  onPressed: () {},
+                  icon: Icon(Icons.delete, color: Colors.grey),
+                ),
+                IconButton(
+                  onPressed: () {},
+                  icon: Icon(Icons.edit, color: Colors.grey),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
