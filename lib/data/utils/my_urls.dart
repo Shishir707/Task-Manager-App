a90a3bfdd70b3c2dfd0241786ffb332d95b2d3fa
Shishir707 <mirshr8@gmail.com>
2025-11-26 21:45:18 +0600

    implement edit task

--- lib/data/utils/my_urls.dart
+++ lib/data/utils/my_urls.dart
@@ -6,7 +6,14 @@ class MyUrls {
   static const String createTask = "$_baseUrl/createTask";
   static const String newTasksUrl = "$_baseUrl/listTaskByStatus/New";
   static const String progressTasksUrl = "$_baseUrl/listTaskByStatus/Progress";
-  static const String cancelledTasksUrl = "$_baseUrl/listTaskByStatus/Cancelled";
-  static const String completedTasksUrl = "$_baseUrl/listTaskByStatus/Completed";
+  static const String cancelledTasksUrl =
+      "$_baseUrl/listTaskByStatus/Cancelled";
+  static const String completedTasksUrl =
+      "$_baseUrl/listTaskByStatus/Completed";
   static const String totalTaskUrl = "$_baseUrl/taskStatusCount";
+
+  static String updateTaskUrl(String id, String status) =>
+      "$_baseUrl/updateTaskStatus/$id/$status";
+
+  static String deleteTaskUrl(String id) => "$_baseUrl/deleteTask/$id";
 }
