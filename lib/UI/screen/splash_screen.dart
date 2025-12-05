df5f4d44b67f5506ddda204655b668bd39c02595
Shishir707 <mirshr8@gmail.com>
2025-12-06 01:42:34 +0600

    add animation logo

--- lib/UI/screen/splash_screen.dart
+++ lib/UI/screen/splash_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 import 'package:task_manager/UI/controller/auth_controller.dart';
 import 'package:task_manager/UI/screen/sign_in_screen.dart';
 import 'package:task_manager/UI/widgets/backgroundScreen.dart';
